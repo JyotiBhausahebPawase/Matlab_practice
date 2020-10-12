1afd8b59feccdb71ae978bce719b6e87ba14bba7
Riyansh <Riyansh@DESKTOP-B9ELENP>
2020-10-12 18:46:56 +0530

    All changes

---
+++ programs/code.m
@@ -0,0 +1,35 @@
+%a = -3;
+
+ %b = -4;
+
+ %Kp = 40;
+
+ %Ki = 4;
+
+ %m = 25;
+
+ %t = [0:0.01:10]';
+
+ %Ts = 0.01;
+
+ %stin = [zeros(floor((length(t)/2)+1),1) ; 5*ones(floor(length(t)/2),1)];
+
+ itr = 200;
+
+ clear yitr
+
+ for i = 1:25:itr
+
+     a = -i;
+
+     sim('lab_first_assignment.slx')
+
+     %yitr(:,i) = yout;
+
+ %     hold on
+
+ %     plot(tout,yout)
+
+ %     legend(num2str(a))
+
+ end
